456b56d063fcaae21dbe0a2b22a38d8a900620eb
Gary Daniels <garydan@microsoft.com>
2023-11-01 17:00:34 -0400

    enable ttt collection for arm systems, disable PAC

--- CollectAudioLogs/CollectAudioLogs.cmd
+++ CollectAudioLogs/CollectAudioLogs.cmd
@@ -1,10 +1,10 @@
-REM Copyright (c) Microsoft Corporation. All rights reserved.
+@rem Copyright (c) Microsoft Corporation. All rights reserved.
 @echo off
 setlocal
 
 set script=%~dp0CollectAudioLogs.ps1
 
-rem look out for WOW64 file system redirection
+@rem look out for WOW64 file system redirection
 set powershell="%windir%\system32\WindowsPowerShell\v1.0\PowerShell.exe"
 if not "%PROCESSOR_ARCHITEW6432%" == "" (
     set powershell="%windir%\sysnative\WindowsPowerShell\v1.0\PowerShell.exe"
